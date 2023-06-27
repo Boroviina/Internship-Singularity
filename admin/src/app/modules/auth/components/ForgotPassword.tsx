25a96b9303d05f90e7561619a09f8dd8e1ecc55d
Milan Vlaski <milan.vlaski321@gmail.com>
2023-06-27 15:55:47 +0200

    iz custom sass izbrisane suvisne stvari i dodate boje koje koristimo

--- admin/src/app/modules/auth/components/ForgotPassword.tsx
+++ admin/src/app/modules/auth/components/ForgotPassword.tsx
@@ -53,11 +53,11 @@ export function ForgotPassword() {
       >
         <div className='text-center mb-10'>
           {/* begin::Title */}
-          <h1 className='text-dark mb-3'>Forgot Password ?</h1>
+          <h1 className='dark-text mb-3'>Forgot Password ?</h1>
           {/* end::Title */}
 
           {/* begin::Link */}
-          <div className='text-gray-400 fw-bold fs-4'>Enter your email to reset your password.</div>
+          <div className='lead-text fw-bold fs-4'>Enter your email to reset your password.</div>
           {/* end::Link */}
         </div>
 
@@ -71,15 +71,15 @@ export function ForgotPassword() {
         )}
 
         {hasErrors === false && (
-          <div className='mb-10 bg-light-info p-8 rounded'>
-            <div className='text-info'>Sent password reset. Please check your email</div>
+          <div className='pwd-reset-bg mb-10 p-8 rounded'>
+            <div className='pwd-reset-text'>Sent password reset. Please check your email</div>
           </div>
         )}
         {/* end::Title */}
 
         {/* begin::Form group */}
         <div className='fv-row mb-10'>
-          <label className='form-label text-dark fw-bolder text-gray-900 fs-6'>Email</label>
+          <label className='form-label dark-text fw-bolder fs-6'>Email</label>
           <input
             type='email'
             placeholder=''
