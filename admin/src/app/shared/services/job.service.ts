28ad87391b914204a9c222e5e98bbbbc9897248e
Tamara Borovina <tamara.borovina2025@gmail.com>
2023-07-05 10:28:08 +0200

    added service for posting job

---
+++ admin/src/app/shared/services/job.service.ts
@@ -0,0 +1,12 @@
+import ApiClient from "./api-client/api-client";
+
+const JOBS_ENDPOINT = '/jobs';
+
+const getJobs = () => {
+    return ApiClient.get(JOBS_ENDPOINT).then(response => response.data);
+}
+
+const createJob = (job) => {
+    return ApiClient.post(JOBS_ENDPOINT, job).then(response => response.data);
+}
+export {getJobs, createJob}
