cbaaede464d0ae40900fb4d1c758b1c515d9ade5
Tamara Borovina <tamara.borovina2025@gmail.com>
2023-07-06 10:31:46 +0200

    job services

--- admin/src/app/shared/services/job.service.ts
+++ admin/src/app/shared/services/job.service.ts
@@ -1,4 +1,5 @@
 import ApiClient from "./api-client/api-client";
+import {JobListing} from "../models/job-listing.model";
 
 const JOBS_ENDPOINT = '/jobs';
 
@@ -6,7 +7,15 @@ const getJobs = () => {
     return ApiClient.get(JOBS_ENDPOINT).then(response => response.data);
 }
 
-const createJob =  (job) => {
-    return ApiClient.post(JOBS_ENDPOINT, job).then(response => response.data);
+const createJob = (job) => {
+    return ApiClient.post(JOBS_ENDPOINT, job).then(response => response.data).then(response => response.map(job => new JobListing(job)));;
 }
 export {getJobs, createJob}
+
+const changeJob = (jobId) => {
+    return ApiClient.put(JOBS_ENDPOINT, jobId).then(response => response.data);
+}
+
+const removeJob=(jobId)=>{
+    return ApiClient.remove(JOBS_ENDPOINT, jobId).then(response=>response.data);
+}
