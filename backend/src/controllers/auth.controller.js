dc030f3eed8a240229ac975cc17ce8bf4719c2af
Milan Vlaski <milan.vlaski321@gmail.com>
2023-07-06 12:00:54 +0200

    auth controller, changed registration to use Employer

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,11 +1,19 @@
 const httpStatus = require('http-status');
 const catchAsync = require('../utils/catchAsync');
 const { authService, userService, tokenService, emailService } = require('../services');
+const employerService = require("../services/employer.service");
+
+// const register = catchAsync(async (req, res) => {
+//   const user = await userService.createUser(req.body);
+//   const tokens = await tokenService.generateAuthTokens(user);
+//   res.status(httpStatus.CREATED).send({ user, tokens });
+// });
 
 const register = catchAsync(async (req, res) => {
   const user = await userService.createUser(req.body);
-  const tokens = await tokenService.generateAuthTokens(user);
-  res.status(httpStatus.CREATED).send({ user, tokens });
+  const employerBody =  {user: user.id, ...req.body};
+  const employer = await employerService.createEmployer(employerBody);
+  res.status(httpStatus.CREATED).send(employer);
 });
 
 const login = catchAsync(async (req, res) => {
