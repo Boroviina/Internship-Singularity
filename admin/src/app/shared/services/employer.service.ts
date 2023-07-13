90617c5a47bfeeb052addd006c9e1d4a774f1e58
Milan Vlaski <milan.vlaski321@gmail.com>
2023-07-13 12:08:19 +0200

    registerEmployer now returns new Employer() as response

--- admin/src/app/shared/services/employer.service.ts
+++ admin/src/app/shared/services/employer.service.ts
@@ -4,9 +4,8 @@ import {Employer} from "../models/employer.model";
 const REGISTER_EMPLOYER_ENDPOINT = '/auth/register-employer';
 
 const registerEmployer = (employer: object): Promise<Employer | null> => {
-    console.log("\n\nREGISTERED EMPLOYER:" + employer + "\n\n");
     return ApiClient.post(REGISTER_EMPLOYER_ENDPOINT, employer)
-        .then(response => response.data);
+        .then(response => new Employer(response.data));
 }
 
 export {registerEmployer};
