ffc3e3380a2c9363d9c35716cc00bba815a8f984
Milan Vlaski <milan.vlaski321@gmail.com>
2023-07-20 13:33:00 +0200

    fixed layout of Job

--- user/src/components/Job.tsx
+++ user/src/components/Job.tsx
@@ -4,28 +4,28 @@ const logo = require('./logo-fb.jpg');
 
 const Job = () => {
     return (
-        <article className="d-flex p-3 flex-column flex-sm-row ">
-            <div className="d-flex">
-                <a href="#employer-info">
+        <article className="d-flex w-100 p-3">
+                <a className="me-3" href="#employer-info">
                     <img src={logo} style={{height: "3.5rem"}} alt={"company logo"}/>
                 </a>
-                <div className="ms-3 flex-grow-1">
+            <div className="d-flex flex-column flex-sm-row">
+                <div className="flex-grow-1 me-3">
                     <h3>Job title</h3>
                     <p className="fw-semibold">
-                        <span>Facebook</span>
+                        <span>Facebook </span>
                         <span className="text-black-50">
-                            . Los Angeles . 50$ - 75$ . About a week ago (week ago)  . Niska . 50$
+                            . Los Angeles . 50$ - 75$ . About a week ago (week ago) . Part time . Until the end of March
                         </span>
                     </p>
                 </div>
-            </div>
-            <div className="d-flex align-items-center gap-2" style={{marginLeft: "4.4rem"}}>
-                <button type="button" className="btn btn-outline-primary">
-                    Save
-                </button>
+            <div className="d-flex align-items-center gap-2">
                 <button type="button" className="btn btn-primary">
                     Apply
                 </button>
+                <button type="button" className="btn btn-outline-primary">
+                    Save
+                </button>
+            </div>
             </div>
 
         </article>
