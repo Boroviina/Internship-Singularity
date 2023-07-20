9e4edeced8d941d99086b6e8f9d3ba8fb8281af5
Milan Vlaski <milan.vlaski321@gmail.com>
2023-07-20 10:12:40 +0200

    code formatting

--- user/src/pages/JobListingPage.tsx
+++ user/src/pages/JobListingPage.tsx
@@ -13,7 +13,9 @@ const JobListingPage = () => {
         <body>
         <header className={`mb-5 ${styles['hero']}`}>
             <div className={`text-center ${styles['overlay']}`}>
-                <h1 className='display-2 text-light' style={{paddingTop: '150px'}}>Search for jobs</h1>
+                <h1 className='display-2 text-light' style={{paddingTop: '150px'}}>
+                    Search for jobs
+                </h1>
             </div>
         </header>
         <main id="jobs-and-filter">
@@ -23,7 +25,9 @@ const JobListingPage = () => {
                         <div className="align-items-center mb-2">
                             <FontAwesomeIcon icon={faFilter}
                                              style={{color: "#198754", height: 30}}/>
-                            <h5 className="d-inline align-baseline">Filter jobs</h5>
+                            <h5 className="d-inline align-baseline">
+                                Filter jobs
+                            </h5>
                         </div>
                             <div className={`container d-flex flex-column gap-3 p-3 filter ${styles['filter']}`}>
                                 <CheckboxGroup name="Job Type 1" filters={filters1}/>
@@ -38,6 +42,7 @@ const JobListingPage = () => {
                 <section className="col-lg-9 col-md-8">
                     <div className="row row-gap-lg-2">
                         <div style={{background: "#54a", width: "100%", height: "120px"}}></div>
+
                         <div style={{background: "#54a", width: "100%", height: "120px"}}></div>
                         <div style={{background: "#54a", width: "100%", height: "120px"}}></div>
                     </div>
