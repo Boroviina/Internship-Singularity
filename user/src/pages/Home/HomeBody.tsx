f6c02dbeab22d4a5355e9dd45bc53060e4461c33
Boroviina <117476003+Boroviina@users.noreply.github.com>
2023-07-21 10:31:18 +0200

    Search Job component created

--- user/src/pages/Home/HomeBody.tsx
+++ user/src/pages/Home/HomeBody.tsx
@@ -3,7 +3,7 @@ import {CarouselCustom} from "./Carousel/CarouselCustom";
 import {SearchJob} from "./SearchJob/SearchJob";
 
 export function HomeBody() {
-    return <div className={'mt-1 card'}>
+    return <div className={'mt-1 '}>
         {/*<div className={'position-relative d-block'}>*/}
         {/*    <div className={'position-relative d-block overflow-hidden m-0 p-0'}>*/}
         {/*        <div className={'position-relative top-0 d-block mx-auto opacity-25 '}*/}
--- user/src/pages/Home/SearchJob/SearchJob.tsx
+++ user/src/pages/Home/SearchJob/SearchJob.tsx
@@ -1,29 +1,32 @@
 import React from "react";
 import classes from './SearchJob.module.css'
 import background from './bgr.jpg'
+import styles from '../Carousel/Carousel.module.css'
 
 export function SearchJob() {
-    return <div className={'card position-relative my-1 '}>
-        <div className={'bgi-no-repeat bgi-position-center d-flex justify-content-end '}
-             style={{backgroundImage: `url(${background})`, height: '50rem', backgroundSize: 'cover'}}>
+    return <div className={'card position-relative  '}>
+        <div className={`bgi-no-repeat bgi-position-center d-flex justify-content-end ${styles.jobSearchHeigh}`}
+             style={{backgroundImage: `url(${background})`, backgroundSize: 'cover'}}>
             <div className={'container d-flex flex-column justify-content-end mt-5'}>
                 <div className={'row d-flex justify-content-end'}>
                     <h1 className={'display-1 fw-bold'}>Find the <br/> most exciting <br/> startup jobs</h1>
                 </div>
                 <div className={'col'}>
-
-                    <div className={'row'}>
+                    <div className={' row'}>
                         <div className={'col-xl-8'}>
-                            <form action="" className={'input-group d-flex flex-row mt-5'}>
+                            <form action="" className={'input-group d-flex flex-column flex-sm-row mt-5'}>
                                 <input type="text" placeholder={'Job title or keyword'} tabIndex={0}
-                                       className={'form-control col-4 '}/>
-                                <select className="form-select" id="floatingSelectDisabled"
+                                       className={'form-control w-auto mt-sm-0 col-sm-4 col-12 '}/>
+                                <select className="form-select mt-1 mt-sm-0 w-auto col-12 col-sm-4" id="floatingSelectDisabled"
                                         aria-label="Floating label disabled select example">
-                                    <option selected>Open this select menu</option>
-                                    <option value="1">One</option>
-                                    <option value="2">Two</option>
-                                    <option value="3">Three</option>
+                                    <option selected>Choose region</option>
+                                    <option value="1">Balcan countries</option>
+                                    <option value="2">Europe</option>
+                                    <option value="3">Other world countries</option>
                                 </select>
+                                <button className={`mt-1 mt-sm-0 col-12 col-sm-2 ${classes.buttonStyle}`}>
+                                    Find Job
+                                </button>
                             </form>
                         </div>
                     </div>
