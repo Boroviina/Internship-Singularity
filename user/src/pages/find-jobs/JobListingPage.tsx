5ca04dd5197df5ea1cd2e4b144e8f0eb15daef12
Milan Vlaski <milan.vlaski321@gmail.com>
2023-07-24 13:53:32 +0200

    styled JobListing

--- user/src/pages/find-jobs/JobListingPage.tsx
+++ user/src/pages/find-jobs/JobListingPage.tsx
@@ -55,11 +55,24 @@ const JobListingPage = () => {
                                 </select>
                             </div>
                         </div>
+                        <div>
                             <Job/>
                             <Job/>
                             <Job/>
                             <Job/>
                             <Job/>
+                        </div>
+                        <nav aria-label="Job search pages">
+                            <ul className="pagination justify-content-center">
+                                <li className="page-item disabled">
+                                    <li className="page-item"><a className="page-link" href="#">Previous</a></li>
+                                </li>
+                                <li className="page-item"><a className="page-link" href="#">1</a></li>
+                                <li className="page-item"><a className="page-link" href="#">2</a></li>
+                                <li className="page-item"><a className="page-link" href="#">3</a></li>
+                                <li className="page-item"><a className="page-link" href="#">Next</a></li>
+                            </ul>
+                        </nav>
                     </section>
                 </div>
             </div>
