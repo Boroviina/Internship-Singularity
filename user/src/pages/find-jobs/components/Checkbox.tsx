eb10da9bbee4c6c6f4178f77787ed81bf436b83e
Milan Vlaski <milan.vlaski321@gmail.com>
2023-07-25 14:45:28 +0200

    increased size of checkboxes

--- user/src/pages/find-jobs/components/Checkbox.tsx
+++ user/src/pages/find-jobs/components/Checkbox.tsx
@@ -3,7 +3,8 @@ import React from 'react';
 const Checkbox = ({name, id}) => {
     return (
         <div className="form-check d-flex align-items-center">
-            <input className="form-check-input check flex-shrink-0" type="checkbox" id={id}/>
+            <input className="form-check-input check flex-shrink-0" type="checkbox" id={id}
+                   style={{width: "20px", height: "20px"}}/>
             <label className="form-check-label fs-5 ms-2" htmlFor={id}>
                 {name}
             </label>
