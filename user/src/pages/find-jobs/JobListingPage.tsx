c9bdfcfeee64ebbf8126e30ca80d4dcc8ca4f046
Milan Vlaski <milan.vlaski321@gmail.com>
2023-07-26 12:30:10 +0200

    extracted pagination to component

--- user/src/pages/find-jobs/JobListingPage.tsx
+++ user/src/pages/find-jobs/JobListingPage.tsx
@@ -11,68 +11,62 @@ import generalBtn from "../Home/GeneralButton.module.css"
 import Search from "./components/Search";
 import Filters from "./components/Filters";
 import SortBy from "./components/SortBy";
+import {Pagination} from "react-bootstrap";
 
-const JobListingPage = () => {
-    const specialization = ["Finance and accounting", "Legal", "Technology",
-    "Administrative & customer support", "Marketing & creative"];
-    const employmentType = ["Full time", "Part time", "Internship", "Contract", "Temporary"];
-    const remote = ["Remote", "Hybrid"];
-    const experienceLevel = ["No experience", "Entry level", "Mid level", "Senior level"];
-    const educationLevel = ["Not required", "College", "Associates", "Bachelors", "Masters", "Doctorate"];
-    const sortByCategories = ["Relevance", "Date", "Pay"];
-    return (
-        <>
-            <Header/>
-            <body>
-            <header className={`mb-5 ${styles.hero}`}>
-                <div className={`text-center ${styles.overlay}`}>
-                    <Search />
-                </div>
-            </header>
-            <main id="jobs-and-filter">
-                <div className="container">
-                    <div className="row">
-                        <section className="col-lg-3 col-md-4 order-2 order-md-1">
-                            <Filters>
-                                <CheckboxGroup name="Specialization" filters={specialization}/>
-                                    <Dropdown name="Remote" filters={remote} />
+class JobListingPage extends React.Component {
+    render() {
+        const specialization = ["Finance and accounting", "Legal", "Technology",
+            "Administrative & customer support", "Marketing & creative"];
+        const employmentType = ["Full time", "Part time", "Internship", "Contract", "Temporary"];
+        const remote = ["Remote", "Hybrid"];
+        const experienceLevel = ["No experience", "Entry level", "Mid level", "Senior level"];
+        const educationLevel = ["Not required", "College", "Associates", "Bachelors", "Masters", "Doctorate"];
+        const sortByCategories = ["Relevance", "Date", "Pay"];
+        return (
+            <>
+                <Header/>
+                <body>
+                <header className={`mb-5 ${styles.hero}`}>
+                    <div className={`text-center ${styles.overlay}`}>
+                        <Search/>
+                    </div>
+                </header>
+                <main id="jobs-and-filter">
+                    <div className="container">
+                        <div className="row">
+                            <section className="col-lg-3 col-md-4 order-2 order-md-1">
+                                <Filters>
+                                    <CheckboxGroup name="Specialization" filters={specialization}/>
+                                    <Dropdown name="Remote" filters={remote}/>
                                     <CheckboxGroup name="Employment type" filters={employmentType}/>
                                     <Dropdown name="Experience level" filters={experienceLevel}/>
-                                    <Dropdown name="Education level" filters={educationLevel} />
-                            </Filters>
-                        </section>
-                        <section className="col-lg-9 col-md-8 order-1 order-md-2">
-                            <div className="d-flex align-items-center justify-content-between px-2">
-                                <div className="text-muted fs-5">
-                                    Results: {24}
-                                </div>
+                                    <Dropdown name="Education level" filters={educationLevel}/>
+                                </Filters>
+                            </section>
+                            <section className="col-lg-9 col-md-8 order-1 order-md-2">
+                                <div className="d-flex align-items-center justify-content-between px-2">
+                                    <div className="text-muted fs-5">
+                                        Results: {24}
+                                    </div>
                                     <SortBy categories={sortByCategories}/>
-                            </div>
-                            <div className="jobs my-2">
-                                <Job/>
-                                <Job/>
-                                <Job/>
-                                <Job/>
-                                <Job/>
-                            </div>
-                            <nav aria-label="Job search pages">
-                                <ul className="pagination justify-content-center">
-                                    <li className="page-item disabled"><a className="page-link" href="#">Previous</a>
-                                    </li>
-                                    <li className="page-item"><a className="page-link" href="#">1</a></li>
-                                    <li className="page-item"><a className="page-link" href="#">2</a></li>
-                                    <li className="page-item"><a className="page-link" href="#">3</a></li>
-                                    <li className="page-item"><a className="page-link" href="#">Next</a></li>
-                                </ul>
-                            </nav>
-                        </section>
+                                </div>
+                                <div className="jobs my-2">
+                                    <Job/>
+                                    <Job/>
+                                    <Job/>
+                                    <Job/>
+                                    <Job/>
+                                </div>
+                                <Pagination />
+                            </section>
+                        </div>
                     </div>
-                </div>
-            </main>
-            </body>
-            <Footer/>
-        </>
-    );
-};
+                </main>
+                </body>
+                <Footer/>
+            </>
+        );
+    }
+}
 
 export default JobListingPage;
