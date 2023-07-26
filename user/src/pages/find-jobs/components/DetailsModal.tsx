a023de44efc710824f70a462a826617de0c2bba0
Milan Vlaski <milan.vlaski321@gmail.com>
2023-07-26 17:30:22 +0200

    refactored inline styles to css modules

--- user/src/pages/find-jobs/components/DetailsModal.tsx
+++ user/src/pages/find-jobs/components/DetailsModal.tsx
@@ -1,25 +1,23 @@
 import React from 'react';
 import {Button, Modal} from "react-bootstrap";
-import styles from "./JobListing.module.css";
-import logo from "./../img/logo-fb.jpg"
+import styles from './Details.module.css';
+import logo from './../img/logo-fb.jpg'
 
 const DetailsModal = ({showDetails, close}) => {
     return (
         <Modal show={showDetails} onHide={close}>
             <Modal.Header className="mx-auto">
-                <div className="d-flex flex-column align-items-center" style={{position: "relative", bottom: "1.5rem"}}>
+                <div className={`d-flex flex-column align-items-center ${styles.detailsHeader}`}>
                 <img src={logo} className={`${styles.logo}`}
-                     style={{height: "5rem", width: "5rem", border: "1px solid #c8d",
-                         borderRadius: "5px", background: "#fcfcfc"}}
                      alt={"Company Logo"}/>
-                <h3 className="mb-1" style={{letterSpacing: "3px"}}>Facebook</h3>
+                <h3 className={`mb-1 ${styles.companyName}`}>Facebook</h3>
                     <div className="d-flex justify-content-around gap-2">
                         <span className="h3 fw-bold">Frontend developer</span>
                         <span className="h3 fw-bold text-success">50$ - 75$</span>
                     </div>
                 </div>
-                <button type="button" className="btn-close" onClick={close} aria-label="Close"
-                    style={{position: "absolute", top: "17px", right: "19px"}}></button>
+                <button type="button" className={`btn-close ${styles.xClose}`} onClick={close}
+                        aria-label="Close"></button>
             </Modal.Header>
             <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
             <Modal.Footer>
