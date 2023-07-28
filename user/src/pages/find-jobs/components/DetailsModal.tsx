798be117343d65a6272d1c545febdd0793441a3d
Milan Vlaski <milan.vlaski321@gmail.com>
2023-07-28 15:43:59 +0200

    small change

--- user/src/pages/find-jobs/components/DetailsModal.tsx
+++ user/src/pages/find-jobs/components/DetailsModal.tsx
@@ -24,7 +24,7 @@ const JobInfo = ({icon, title, mainData, secondaryData}: JobInfoProps) => {
                 <div className="fw-bold fs-5 mb-1">{title}:</div>
                 <span className="p-1 fw-semibold" style={{
                     borderRadius: "3px", background: "#e5e6ee"
-                    , color: "#7f8593"
+                    ,color: "#7f8593"
                 }}>
                     {mainData + (secondaryData ? " - " + secondaryData : "")}
                 </span>
