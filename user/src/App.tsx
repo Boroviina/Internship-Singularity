5a4f5277f96fc1ee74c8a696403089f86b44f904
Boroviina <117476003+Boroviina@users.noreply.github.com>
2023-07-28 12:03:36 +0200

    Application running features installed

--- user/src/App.tsx
+++ user/src/App.tsx
@@ -1,16 +1,21 @@
-import {Suspense} from 'react'
+import {Suspense, useState} from 'react'
 import {Outlet} from 'react-router-dom'
 import {LayoutSplashScreen} from "./layout/core";
 import {AuthInit} from './modules/auth'
 
+
 const App = () => {
-  return (
-      <Suspense fallback={<LayoutSplashScreen/>}>
-          <AuthInit>
-              <Outlet/>
-          </AuthInit>
-      </Suspense>
-  )
+
+
+    return (
+        <Suspense fallback={<LayoutSplashScreen/>}>
+            <AuthInit>
+                <div className={'bg-body'}>
+                    <Outlet/>
+                </div>
+            </AuthInit>
+        </Suspense>
+    )
 }
 
 export {App}
--- user/src/index.tsx
+++ user/src/index.tsx
@@ -2,18 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import axios from 'axios'
 import './index.css';
-import {AppRoutes} from "./routing/AppRoutes";
-import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap';
+import './customStyle/custom.scss'
+import {AppRoutes} from "./routing/AppRoutes";
 import {AuthProvider, setupAxios} from "./modules/auth";
+import {ThemeContextProvide} from "./theme-context/ThemeContext";
 
 setupAxios(axios)
 
+
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
 root.render(
-    <AuthProvider>
-        <AppRoutes/>
-    </AuthProvider>
+    <ThemeContextProvide>
+        <AuthProvider>
+            <AppRoutes/>
+        </AuthProvider>
+    </ThemeContextProvide>
 );
