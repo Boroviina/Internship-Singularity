5f3b563f92882325ee9b82eadd9e4d34ab985553
Milan Vlaski <milan.vlaski321@gmail.com>
2023-08-01 13:44:37 +0200

    job card compiles with no errors even if a Job object, with no fields, is passed to it

--- user/src/pages/find-jobs/DetailsModal.tsx
+++ user/src/pages/find-jobs/DetailsModal.tsx
@@ -15,7 +15,7 @@ interface DetailsProps {
 }
 
 const DetailsModal = ({job, showDetails, close}: DetailsProps) => {
-    const requirements = job.requirementsModel;
+    const requirements = job.requirementsModel ? job.requirementsModel : null;
     return (
         <Modal show={showDetails} onHide={close} size="lg">
 
@@ -39,41 +39,42 @@ const DetailsModal = ({job, showDetails, close}: DetailsProps) => {
                         {job.employmentType
                             && <JobInfo title="Employment type" mainData={job.employmentType} icon="briefcase"/>}
                         <hr/>
-                            {job.appDeadline
-                                && <JobInfo title="Application deadline"
-                                            mainData={job.appDeadline.toLocaleDateString('en-us', {
-                                                month: "long",
-                                                day: "numeric"
-                                            })}
-                                            icon="hourglass-half"/>}
-                            {job.cv !== undefined && job.cv
-                                && <JobInfo title="CV" mainData={job.cv ? "Required" : "Not required"}
-                                            icon="image-portrait"/>}
-                            {job.coverLetter !== undefined && job.coverLetter
-                                && <JobInfo title="Cover letter" mainData={job.coverLetter ? "Required" : "Not required"}
-                                         icon="envelope-open-text"/>}
-                            {job.positionsNum // ovo ne radi sa && :'D
-                                ? <JobInfo title="Open positions" mainData={job.positionsNum.toString() + " left"}
-                                            icon="user-group" /> : null}
+                        {job.appDeadline
+                            && <JobInfo title="Application deadline"
+                                        mainData={job.appDeadline.toLocaleDateString('en-us', {
+                                            month: "long",
+                                            day: "numeric"
+                                        })}
+                                        icon="hourglass-half"/>}
+                        {job.cv !== undefined && job.cv
+                            && <JobInfo title="CV" mainData={job.cv ? "Required" : "Not required"}
+                                        icon="image-portrait"/>}
+                        {job.coverLetter !== undefined && job.coverLetter
+                            && <JobInfo title="Cover letter" mainData={job.coverLetter ? "Required" : "Not required"}
+                                        icon="envelope-open-text"/>}
+                        {job.positionsNum // ovo ne radi sa && :'D
+                            ? <JobInfo title="Open positions" mainData={job.positionsNum.toString() + " left"}
+                                       icon="user-group"/> : null}
                     </section>
 
-                    <section className={styles.qualifications}>
-                        {requirements.specialization
-                            &&
-                            <JobInfo title="Area of expertise" mainData={requirements.specialization} icon="user-tie"/>}
-                        {requirements.experience
-                            && <JobInfo title="Experience" mainData={requirements.experience} icon="glasses"/>}
-                        {requirements.education
-                            && <JobInfo title="Education" mainData={requirements.education} icon="graduation-cap"/>}
-                        {requirements.skills
-                            && <JobInfo title="Skills" mainData={requirements.skills} icon="pencil"/>}
-                        {requirements.driverLicence
-                            && <JobInfo title="Driving license"
-                                        mainData={requirements.driverLicence ? "Required" : "Not required"}
-                                        icon="car-side"/>}
-                        {requirements.language
-                            && <JobInfo title="Language" mainData={requirements.language} icon="earth-americas"/>}
-                    </section>
+                    {requirements
+                        && <section className={styles.qualifications}>
+                            {requirements.specialization
+                                && <JobInfo title="Area of expertise" mainData={requirements.specialization}
+                                            icon="user-tie"/>}
+                            {requirements.experience
+                                && <JobInfo title="Experience" mainData={requirements.experience} icon="glasses"/>}
+                            {requirements.education
+                                && <JobInfo title="Education" mainData={requirements.education} icon="graduation-cap"/>}
+                            {requirements.skills
+                                && <JobInfo title="Skills" mainData={requirements.skills} icon="pencil"/>}
+                            {requirements.driverLicence
+                                && <JobInfo title="Driving license"
+                                            mainData={requirements.driverLicence ? "Required" : "Not required"}
+                                            icon="car-side"/>}
+                            {requirements.language
+                                && <JobInfo title="Language" mainData={requirements.language} icon="earth-americas"/>}
+                        </section>}
 
                     <section className={styles.jobDescription}>
                         {job.description
