e7256b6f85babed1c6c0e0a575b34e5d93a0f7b4
Milan Vlaski <milan.vlaski321@gmail.com>
2023-08-03 15:47:03 +0200

    deleted jest

--- backend/tests/unit/models/employer.model.test.js
+++
@@ -1,6 +0,0 @@
-const {Employer} = require("../../../src/models");
-
-test('query should return result', async () => {
-  const employer = await Employer.findOne({adminUser: "64a6c621d71c1b00fc6e6867"});
-  expect(employer._id).toBe("64a6c621d71c1b00fc6e6867");
-  });
