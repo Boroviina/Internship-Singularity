e5ce3146b2923ca27555944f15b6a6b587b7f290
Milan Vlaski <milan.vlaski321@gmail.com>
2023-08-07 10:36:17 +0200

    removed unused apis from job service

--- user/src/shared/services/job.service.ts
+++ user/src/shared/services/job.service.ts
@@ -6,25 +6,20 @@ const JOBS_ENDPOINT = '/jobs';
 const getJobs = (): Promise<JobListing[] | null> => {
     return ApiClient.get(JOBS_ENDPOINT, '&populate=employer,requirements')
         .then(response => response.data)
-        .then(data => data.results.map(job => new JobListing(job)))
+        .then(data => {
+            console.log(data);
+            return data.results.map(job => new JobListing(job));
+        });
 }
 
 const getJob = (jobId: string): Promise<JobListing | null> => {
     return ApiClient.get(`${JOBS_ENDPOINT}/${jobId}`)
         .then(response => response.data)
-        .then(data => new JobListing(data))
-}
-
-const createJob = (job: object): Promise<JobListing | null> => {
-    return ApiClient.post(JOBS_ENDPOINT, job)
-        .then(response => response.data)
-}
-const changeJob = (jobId: string): Promise<JobListing | null> => {
-    return ApiClient.put(JOBS_ENDPOINT, jobId).then(response => response.data);
+        .then(data => new JobListing(data));
 }
 
 const removeJob = (jobId): Promise<JobListing | null>  => {
     return ApiClient.remove(JOBS_ENDPOINT, jobId).then(response => response.data);
 }
-export {getJob, getJobs, createJob, changeJob, removeJob}
+export {getJob, getJobs, removeJob}
 
