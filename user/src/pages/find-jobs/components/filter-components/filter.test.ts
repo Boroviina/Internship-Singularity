963a43d84ad46850c26b4a6854792b98cac07d44
Milan Vlaski <milan.vlaski321@gmail.com>
2023-08-10 12:10:49 +0200

    filters tested and functional

--- user/src/pages/find-jobs/components/filter-components/filter.test.ts
+++ user/src/pages/find-jobs/components/filter-components/filter.test.ts
@@ -8,7 +8,8 @@ describe('Filter jobs', () => {
         const job = new JobListing({requirements: new RequirementsModel({}),});
         const filters = new JobFilters([], [], [], [], []);
 
-        expect(filterJobs([job], filters)).toEqual([job]);
+        expect(filterJobs([job], filters))
+            .toEqual([job]);
     });
 
     let technologyJob;
@@ -19,7 +20,8 @@ describe('Filter jobs', () => {
     test('should return job if matches filter', () => {
         const filters = new JobFilters(["Technology"], [], [], [], []);
 
-        expect(filterJobs([technologyJob], filters)).toEqual([technologyJob]);
+        expect(filterJobs([technologyJob], filters))
+            .toEqual([technologyJob]);
     });
 
     test('should return job that matches specialization and remote', () => {
@@ -29,7 +31,8 @@ describe('Filter jobs', () => {
         });
         const filters = new JobFilters(["Technology"], ["Remote"], [], [], []);
 
-        expect(filterJobs([matchingJob, technologyJob], filters)).toEqual([matchingJob]);
+        expect(filterJobs([matchingJob, technologyJob], filters))
+            .toEqual([matchingJob]);
     });
 
     test('should return job that matches specialization and experience (both are requirements)', () => {
@@ -38,7 +41,8 @@ describe('Filter jobs', () => {
         });
         const filters = new JobFilters(["Technology"], [], [], ["High level"], []);
 
-        expect(filterJobs([matchingJob, technologyJob], filters)).toEqual([matchingJob]);
+        expect(filterJobs([matchingJob, technologyJob], filters))
+            .toEqual([matchingJob]);
     });
 
     test('should return job that matches employmentType', () => {
@@ -46,20 +50,38 @@ describe('Filter jobs', () => {
         const nonMatchingJob = new JobListing({requirements: new RequirementsModel()});
         const filters = new JobFilters([], [], ["Full time"], [], []);
 
-        expect(filterJobs([matchingJob, nonMatchingJob], filters)).toEqual([matchingJob]);
+        expect(filterJobs([matchingJob, nonMatchingJob], filters))
+            .toEqual([matchingJob]);
     });
 
     test('should return empty array if doesnt match filter', () => {
-        const job = new JobListing({requirements: new RequirementsModel({specialization: "Technology"}),});
         const filters = new JobFilters(["Finance"], [], [], [], []);
 
-        expect(filterJobs([job], filters)).toEqual([]);
+        expect(filterJobs([technologyJob], filters))
+            .toEqual([]);
     });
 
     test('should return empty array if the job attribute being filtered is undefined', () => {
         const job = new JobListing({remote: undefined, requirements: new RequirementsModel()});
         const filters = new JobFilters([], ["Remote"], [], [], []);
 
-        expect(filterJobs([job], filters)).toEqual([]);
+        expect(filterJobs([job], filters))
+            .toEqual([]);
+    });
+    test('should return two jobs, with two different specializations, when those two specializations are filtered for', () => {
+        const financeJob= new JobListing({requirements: new RequirementsModel({specialization: "Finance"})});
+        const filters = new JobFilters(["Finance", "Technology"], [], [], [], []);
+
+        expect(filterJobs([technologyJob, financeJob], filters))
+            .toEqual([technologyJob, financeJob]);
+    });
+
+    test('should exclude job which doesnt match any filters', () => {
+        const financeJob= new JobListing({requirements: new RequirementsModel({specialization: "Finance"})});
+        const marketingJob = new JobListing({requirements: new RequirementsModel({specialization: "Marketing"})});
+        const filters = new JobFilters(["Finance", "Technology"], [], [], [], []);
+
+        expect(filterJobs([technologyJob, financeJob, marketingJob], filters))
+            .toEqual([technologyJob, financeJob]);
     });
 });
