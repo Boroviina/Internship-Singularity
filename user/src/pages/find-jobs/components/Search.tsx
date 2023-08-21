0461deff61f72dc9aa8b4f54b781fc658e4799d2
Milan Vlaski <milan.vlaski321@gmail.com>
2023-08-21 16:05:43 +0200

    extracted component

--- user/src/pages/find-jobs/components/Search.tsx
+++ user/src/pages/find-jobs/components/Search.tsx
@@ -1,33 +1,14 @@
 import React, {useState} from 'react';
+import SearchBar from "./SearchBar";
 
 
 const Search = ({search}) => {
-    const [title, setTitle] = useState("");
-    const [location, setLocation] = useState("");
-
-    const onTitleChanged = (e) => setTitle(e.target.value);
-    const onLocationChanged = (e) => setLocation(e.target.value);
-
-    const submit = () => search(title, location);
-
     return (
         <div className="container">
             <h1 className='display-2 text-light' style={{paddingTop: '150px'}}>
                 Search for jobs
             </h1>
-            <div className="input-group input-group-lg d-flex flex-column flex-sm-row flex-nowrap mt-4">
-                <div className="form-floating w-auto">
-                    <input type="text" className="form-control fs-6" id="keywordSearch"
-                           placeholder="Job title or keywords" aria-label="Keyword search" value={title} onChange={onTitleChanged}/>
-                    <label htmlFor="keywordSearch">Job title</label>
-                </div>
-                <div className="form-floating w-auto">
-                    <input type="text" className="form-control fs-6" id="locationSearch"
-                           placeholder="Address, city or country" aria-label="Location search" value={location} onChange={onLocationChanged}/>
-                    <label htmlFor="locationSearch">Country, city or region</label>
-                </div>
-                <button className={'btn btn-pink d-inline-block overflow-visible'} onClick={submit}>Search</button>
-            </div>
+            <SearchBar search={search}/>
         </div>
     );
 };
